f7c4525e6b5130c2b043b5a2a334e965030599d6
Frizlab <fload@me.com>
2015-03-04 15:40:10 +0100

    Main is now in Swift.

---
+++ CPU Indicator/main.swift
@@ -0,0 +1,13 @@
+/*
+ * main.swift
+ * CPU Indicator
+ *
+ * Created by François LAMBOLEY on 04/03/15.
+ * Copyright (c) 2015 Frost Land. All rights reserved.
+ */
+
+import Cocoa
+
+
+
+NSApplicationMain(Process.argc, Process.unsafeArgv)
