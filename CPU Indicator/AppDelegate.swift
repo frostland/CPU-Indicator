aac6a834bbfd71feb2ebaa9841601909c6c77e22
Frizlab <fload@me.com>
2015-06-13 19:34:09 +0200

    Project re-created from scratch

---
+++ CPU Indicator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CPU Indicator
+//
+//  Created by François Lamboley on 13/06/15.
+//  Copyright © 2015 Frost Land. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
