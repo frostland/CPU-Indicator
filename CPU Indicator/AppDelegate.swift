806a31bc47e43e0cf682311e193ad44047bba617
Frizlab <fload@me.com>
2015-06-21 18:29:12 +0200

    Intro view implemented.

--- CPU Indicator/AppDelegate.swift
+++ CPU Indicator/AppDelegate.swift
@@ -1,26 +1,25 @@
-//
-//  AppDelegate.swift
-//  CPU Indicator
-//
-//  Created by François Lamboley on 13/06/15.
-//  Copyright © 2015 Frost Land. All rights reserved.
-//
+/*
+ * AppDelegate.swift
+ * CPU Indicator
+ *
+ * Created by François Lamboley on 13/06/15.
+ * Copyright © 2015 Frost Land. All rights reserved.
+ */
 
 import Cocoa
 
+
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+	
 	func applicationDidFinishLaunching(aNotification: NSNotification) {
-		// Insert code here to initialize your application
+		let wc = NSStoryboard(name: "Main", bundle: nil).instantiateControllerWithIdentifier("IntroWindow") as! NSWindowController
+		wc.showWindow(self)
 	}
-
+	
 	func applicationWillTerminate(aNotification: NSNotification) {
 		// Insert code here to tear down your application
 	}
 
-
 }
-
---
+++ CPU Indicator/IntroViewController.swift
@@ -0,0 +1,29 @@
+/*
+ * IntroViewController.swift
+ * CPU Indicator
+ *
+ * Created by François Lamboley on 13/06/15.
+ * Copyright © 2015 Frost Land. All rights reserved.
+ */
+
+import Cocoa
+
+
+
+class IntroViewController: NSViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Do any additional setup after loading the view.
+	}
+	
+	@IBAction func okButtonTapped(sender: AnyObject?) {
+		self.view.window?.windowController?.close()
+	}
+	
+	@IBAction func prefsButtonTapped(sender: AnyObject?) {
+		
+	}
+	
+}
