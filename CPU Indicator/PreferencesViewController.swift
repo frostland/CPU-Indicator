9d91728a33ae2f603701c26ac069cea8feef3659
Frizlab <fload@me.com>
2015-08-17 22:36:31 +0200

    First version of the tab view in the preferences + design of first pref pane implemented.
    Todo 1: On a given pref tab, the window size can be changed when it shouldn't.
    Todo 2: When the size of a pref tab is modified, the new size is not remembered.

--- CPU Indicator/PreferencesViewController.swift
+++ CPU Indicator/PreferencesViewController.swift
@@ -1,16 +1,18 @@
 /*
-* PreferencesViewController.swift
-* CPU Indicator
-*
-* Created by François Lamboley on 7/18/15.
-* Copyright © 2015 Frost Land. All rights reserved.
-*/
+ * PreferencesViewController.swift
+ * CPU Indicator
+ *
+ * Created by François Lamboley on 7/18/15.
+ * Copyright © 2015 Frost Land. All rights reserved.
+ */
 
 import Cocoa
 
 
 
-class PreferencesViewController: NSViewController {
+class PreferencesViewController: NSTabViewController {
+	
+	var originalSizes = [String : NSSize]()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -20,6 +22,30 @@ class PreferencesViewController: NSViewController {
 		super.viewWillAppear()
 		
 		AppDelegate.sharedAppDelegate.closeIntroWindow()
+	}
+	
+	override func tabView(tabView: NSTabView, willSelectTabViewItem tabViewItem: NSTabViewItem?) {
+		super.tabView(tabView, willSelectTabViewItem: tabViewItem)
+		
+		if let identifier = tabViewItem?.identifier as? String, v = tabViewItem?.view {
+			if originalSizes[identifier] == nil {
+				originalSizes[identifier] = v.frame.size
+			}
+		}
+	}
+	
+	override func tabView(tabView: NSTabView, didSelectTabViewItem tabViewItem: NSTabViewItem?) {
+		super.tabView(tabView, didSelectTabViewItem: tabViewItem)
+		
+		if let window = self.view.window, identifier = tabViewItem?.identifier as? String, s = self.originalSizes[identifier] {
+			dispatch_after(DISPATCH_TIME_NOW, dispatch_get_main_queue()) { () -> Void in
+				let destSize = window.frameRectForContentRect(NSMakeRect(0, 0, s.width, s.height)).size
+				var windowFrame = window.frame
+				windowFrame.origin.y += windowFrame.size.height - destSize.height
+				windowFrame.size = destSize
+				window.setFrame(windowFrame, display: true, animate: true)
+			}
+		}
 	}
 	
 }
