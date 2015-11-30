7c48f1675f35d8380ede126750fd684c47929fa7
Frizlab <fload@me.com>
2015-11-30 23:08:08 +0100

    Make preview cell actually preview the whole skin in preferences skin selection

--- CPU Indicator/Views and Controllers/SkinView.swift
+++ CPU Indicator/Views and Controllers/SkinView.swift
@@ -54,12 +54,93 @@ class SkinView : NSView {
 		}
 	}*/
 	
-	var sizedSkin: SizedSkin?
+	class PreviewProgress {
+		private static let sharedPreviewProgress = PreviewProgress()
+		private static let kPreviewProgressChangeNotificationName = "SkinView Preview Progress Change Notification"
+		
+		private let previewFPS: Int
+		private let previewTimeBetweenFirstAndLastImage: Int
+		private let nFramesBetweenFirstAndLastImage: Int
+		
+		private var delta = 1
+		private var curProgress = 0
+		
+		convenience init() {
+			self.init(FPS: 15, timeBetweenFirstAndLastImage: 5)
+		}
+		
+		init(FPS: Int, timeBetweenFirstAndLastImage: Int) {
+			previewFPS = FPS
+			previewTimeBetweenFirstAndLastImage = timeBetweenFirstAndLastImage
+			nFramesBetweenFirstAndLastImage = previewFPS * previewTimeBetweenFirstAndLastImage
+		}
+		
+		private var timerUpdatePreviewProgress: NSTimer?
+		private var nPreviewProgressObserver: Int = 0 {
+			didSet {
+				if oldValue > 0 && nPreviewProgressObserver == 0 {
+					/* Let's stop the preview progress observer as nobobdy observes it... */
+					timerUpdatePreviewProgress?.invalidate()
+					timerUpdatePreviewProgress = nil
+				} else if oldValue == 0 && nPreviewProgressObserver > 0 {
+					/* Let's start the preview progress observer: somebody is interested... */
+					timerUpdatePreviewProgress?.invalidate()
+					timerUpdatePreviewProgress = NSTimer.scheduledTimerWithTimeInterval(1.0/Double(previewFPS), target: self, selector: Selector("advancePreviewProgress:"), userInfo: nil, repeats: true)
+				}
+			}
+		}
+		var currentProgress: Float {
+			return Float(curProgress)/Float(nFramesBetweenFirstAndLastImage)
+		}
+		
+		@objc
+		private func advancePreviewProgress(timer: NSTimer!) {
+			curProgress += delta
+			if curProgress >= nFramesBetweenFirstAndLastImage || curProgress <= 0 {
+				delta *= -1
+			}
+			NSNotificationCenter.defaultCenter().postNotificationName(self.dynamicType.kPreviewProgressChangeNotificationName, object: self)
+		}
+	}
+	
+	var sizedSkin: SizedSkin? {
+		didSet {
+			self.setNeedsDisplayInRect(self.bounds)
+		}
+	}
 	
 	/**
-  If non-nil, the progress will be static, else, the shared "preview" progress
-	will be used. */
-	@IBInspectable var progress: Float?
+  If lower than 0, the shared "preview" progress will be used, else the actual
+	given value will be used.
+	
+  Ideally, I would have set the type to Float?, but the property can't be
+	designable if I did so... */
+	@IBInspectable var progress: Float = 0 {
+		didSet {
+			if progress < 0 {
+				assert(previewProgressObserver == nil)
+				++PreviewProgress.sharedPreviewProgress.nPreviewProgressObserver
+				previewProgressObserver = NSNotificationCenter.defaultCenter().addObserverForName(PreviewProgress.kPreviewProgressChangeNotificationName, object: nil, queue: nil, usingBlock: { [weak self] n in
+					self?.setNeedsDisplayInRect(self!.bounds)
+				})
+			} else {
+				removePreviewProgressObserverIfNeeded()
+			}
+		}
+	}
+	private var previewProgressObserver: NSObjectProtocol?
+	
+	deinit {
+		removePreviewProgressObserverIfNeeded()
+	}
+	
+	private func removePreviewProgressObserverIfNeeded() {
+		if let obs = previewProgressObserver {
+			--PreviewProgress.sharedPreviewProgress.nPreviewProgressObserver
+			NSNotificationCenter.defaultCenter().removeObserver(obs, name: PreviewProgress.kPreviewProgressChangeNotificationName, object: nil)
+			previewProgressObserver = nil
+		}
+	}
 	
 	override func drawRect(dirtyRect: NSRect) {
 		guard let sizedSkin = sizedSkin else {
@@ -75,7 +156,7 @@ class SkinView : NSView {
 		)
 		
 		let drawRect = NSRect(origin: p, size: drawnSize)
-		sizedSkin.imageForProgress(0.0).drawInRect(
+		sizedSkin.imageForProgress(progress >= 0 ? progress : PreviewProgress.sharedPreviewProgress.currentProgress).drawInRect(
 			drawRect,
 			fromRect: NSRect(origin: CGPointZero, size: drawnSize),
 			operation: .CompositeSourceOver,
