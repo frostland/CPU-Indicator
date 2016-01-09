59f84bbfdcdc08b63818f2cacd1ef5ed39785335
Frizlab <fload@me.com>
2016-01-09 18:50:23 +0100

    Set skin and scale from prefs in indicator window

---
+++ CPU Indicator/Views and Controllers/IndicatorWindowContentViewController.swift
@@ -0,0 +1,17 @@
+/*
+ * IndicatorWindowContentViewController.swift
+ * CPU Indicator
+ *
+ * Created by François Lamboley on 1/9/16.
+ * Copyright © 2016 Frost Land. All rights reserved.
+ */
+
+import Cocoa
+
+
+
+class IndicatorWindowContentViewController : NSViewController {
+	
+	@IBOutlet var skinView: SkinView!
+	
+}
--- CPU Indicator/Views and Controllers/IndicatorWindowController.swift
+++ CPU Indicator/Views and Controllers/IndicatorWindowController.swift
@@ -11,6 +11,10 @@ import Cocoa
 
 
 class IndicatorWindowController: NSWindowController {
+	private var skinView: SkinView! {
+		return (self.contentViewController as? IndicatorWindowContentViewController)?.skinView
+	}
+	
 	private var observingUDC = false
 	private let observedUDCKeys = [
 		"values.\(kUDK_ShowWindowIndicator)",
@@ -31,63 +35,75 @@ class IndicatorWindowController: NSWindowController {
 	override func windowDidLoad() {
 		super.windowDidLoad()
 		
+		self.window?.opaque = false
+		self.window?.movableByWindowBackground = true
+		self.window?.backgroundColor = NSColor.clearColor()
+		
+		super.windowDidLoad()
+		
 		let udc = NSUserDefaultsController.sharedUserDefaultsController()
 		for keyPath in observedUDCKeys {
-			udc.addObserver(self, forKeyPath: keyPath, options: .Initial, context: nil)
+			udc.addObserver(self, forKeyPath: keyPath, options: [.Initial], context: nil)
 		}
-		AppDelegate.sharedAppDelegate.addObserver(self, forKeyPath: "selectedSkinObjectID", options: [], context: nil)
+		AppDelegate.sharedAppDelegate.addObserver(self, forKeyPath: "selectedSkinObjectID", options: [.Initial], context: nil)
 		observingUDC = true
 	}
 	
 	override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
-		guard let kp = keyPath where observedUDCKeys.contains(kp) && object === NSUserDefaultsController.sharedUserDefaultsController() else {
+		guard let kp = keyPath else {
 			super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
 			return
 		}
 		
-		let prefix = "values."
-		let ud = NSUserDefaults.standardUserDefaults()
-		switch kp.substringFromIndex(prefix.endIndex) {
-		case kUDK_ShowWindowIndicator:
-			if ud.boolForKey(kUDK_FirstRun) {
-				if let window = window {
-					let screenRect = window.screen?.visibleFrame ?? CGRectMake(0, 0, 640, 480)
-					var f = window.frame
-					f.origin.x = screenRect.origin.x + screenRect.size.width  - f.size.width
-					f.origin.y = screenRect.origin.y + screenRect.size.height - f.size.height
-					window.setFrame(f, display: true, animate: false)
+		if observedUDCKeys.contains(kp) && object === NSUserDefaultsController.sharedUserDefaultsController() {
+			let prefix = "values."
+			let ud = NSUserDefaults.standardUserDefaults()
+			switch kp.substringFromIndex(prefix.endIndex) {
+			case kUDK_ShowWindowIndicator:
+				if ud.boolForKey(kUDK_FirstRun) {
+					if let window = window {
+						let screenRect = window.screen?.visibleFrame ?? CGRectMake(0, 0, 640, 480)
+						var f = window.frame
+						f.origin.x = screenRect.origin.x + screenRect.size.width  - f.size.width
+						f.origin.y = screenRect.origin.y + screenRect.size.height - f.size.height
+						window.setFrame(f, display: true, animate: false)
+					}
 				}
+				if ud.boolForKey(kUDK_ShowWindowIndicator) {self.showWindow(self)}
+				else                                       {self.close()}
+			case kUDK_WindowIndicatorLevel:
+				switch WindowIndicatorLevel(rawValue: ud.integerForKey(kUDK_WindowIndicatorLevel)) ?? WindowIndicatorLevel.AboveAll {
+				case .BehindAll:
+					ud.setBool(true, forKey: kUDK_WindowIndicatorClickless)
+					ud.setBool(true, forKey: kUDK_WindowIndicatorLocked)
+					self.window?.level = Int(CGWindowLevelForKey(CGWindowLevelKey.DesktopWindowLevelKey))
+					self.window?.collectionBehavior = [.CanJoinAllSpaces, .Transient]
+				case .Normal:
+					self.window?.level = Int(CGWindowLevelForKey(CGWindowLevelKey.NormalWindowLevelKey))
+					self.window?.collectionBehavior = [.CanJoinAllSpaces, .Managed]
+				case .AboveAll:
+					self.window?.level = Int(CGWindowLevelForKey(CGWindowLevelKey.StatusWindowLevelKey))
+					self.window?.collectionBehavior = [.CanJoinAllSpaces, .Stationary]
+				}
+			case kUDK_WindowIndicatorOpacity:
+				self.window?.alphaValue = CGFloat(ud.floatForKey(kUDK_WindowIndicatorOpacity))
+			case kUDK_WindowIndicatorClickless:
+				self.window?.ignoresMouseEvents = ud.boolForKey(kUDK_WindowIndicatorClickless)
+			case kUDK_WindowIndicatorLocked:
+				self.window?.movable = !ud.boolForKey(kUDK_WindowIndicatorLocked)
+			case kUDK_WindowIndicatorScale:
+				self.updateSkinAndScale()
+			case kUDK_WindowIndicatorDisableShadow:
+				self.window?.hasShadow = !ud.boolForKey(kUDK_WindowIndicatorDisableShadow)
+			case kUDK_WindowIndicatorDecreaseOpacityOnHover:
+				(/*TODO*/)
+			default:
+				fatalError("Unreachable code has been reached!")
 			}
-			if ud.boolForKey(kUDK_ShowWindowIndicator) {self.showWindow(self)}
-			else                                       {self.close()}
-		case kUDK_WindowIndicatorLevel:
-			switch WindowIndicatorLevel(rawValue: ud.integerForKey(kUDK_WindowIndicatorLevel)) ?? WindowIndicatorLevel.AboveAll {
-			case .BehindAll:
-				ud.setBool(true, forKey: kUDK_WindowIndicatorClickless)
-				ud.setBool(true, forKey: kUDK_WindowIndicatorLocked)
-				self.window?.level = Int(CGWindowLevelForKey(CGWindowLevelKey.DesktopWindowLevelKey))
-				self.window?.collectionBehavior = [.CanJoinAllSpaces, .Transient]
-			case .Normal:
-				self.window?.level = Int(CGWindowLevelForKey(CGWindowLevelKey.NormalWindowLevelKey))
-				self.window?.collectionBehavior = [.CanJoinAllSpaces, .Managed]
-			case .AboveAll:
-				self.window?.level = Int(CGWindowLevelForKey(CGWindowLevelKey.StatusWindowLevelKey))
-				self.window?.collectionBehavior = [.CanJoinAllSpaces, .Stationary]
-			}
-		case kUDK_WindowIndicatorOpacity:
-			self.window?.alphaValue = CGFloat(ud.floatForKey(kUDK_WindowIndicatorOpacity))
-		case kUDK_WindowIndicatorClickless:
-			self.window?.ignoresMouseEvents = ud.boolForKey(kUDK_WindowIndicatorClickless)
-		case kUDK_WindowIndicatorLocked:
-			self.window?.movable = !ud.boolForKey(kUDK_WindowIndicatorLocked)
-		case kUDK_WindowIndicatorScale:
-			(/*TODO*/)
-		case kUDK_WindowIndicatorDisableShadow:
-			self.window?.hasShadow = !ud.boolForKey(kUDK_WindowIndicatorDisableShadow)
-		case kUDK_WindowIndicatorDecreaseOpacityOnHover:
-			(/*TODO*/)
-		default:
-			fatalError("Unreachable code has been reached!")
+		} else if kp == "selectedSkinObjectID" && object === AppDelegate.sharedAppDelegate {
+			self.updateSkinAndScale()
+		} else {
+			super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
 		}
 	}
 	
@@ -161,6 +177,34 @@ class IndicatorWindowController: NSWindowController {
 		f.origin.x = screenRect.origin.x + screenRect.size.width - f.size.width
 		f.origin.y = screenRect.origin.y
 		self.window?.setFrame(f, display: true, animate: true)
+	}
+	
+	/* ***************
+	   MARK: - Private
+	   *************** */
+	
+	private func updateSkinAndScale() {
+		let appDelegate = AppDelegate.sharedAppDelegate
+		let context = appDelegate.mainManagedObjectContext
+		let scale = CGFloat(NSUserDefaults.standardUserDefaults().doubleForKey(kUDK_WindowIndicatorScale))
+		
+		var sizedSkinO: SizedSkin?
+		context.performBlockAndWait {
+			guard let skin = (try? context.existingObjectWithID(appDelegate.selectedSkinObjectID)) as? Skin else {
+				return
+			}
+			
+			let w = CGFloat(skin.width), h = CGFloat(skin.height)
+			var size = NSMakeSize(w * scale, h * scale)
+			let f = w / h
+			if size.width  <  9 {size.width  =  9; size.height = size.width  / f}
+			if size.height < 19 {size.height = 19; size.width  = size.height * f}
+			sizedSkinO = SizedSkin(skin: skin, size: size, allowDistortion: false)
+		}
+		guard let sizedSkin = sizedSkinO else {return}
+		self.skinView.sizedSkin = sizedSkin
+		self.window?.setContentSize(sizedSkin.size)
+		self.window?.invalidateShadow()
 	}
 	
 }
