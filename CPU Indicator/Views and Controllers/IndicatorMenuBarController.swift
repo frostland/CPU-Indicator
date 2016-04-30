3aa45bcc9a1ef6af1610859d0ece2e5499c2df5a
Frizlab <fload@me.com>
2016-04-30 19:23:42 +0200

    Show indicator image when needed in menu
    Mixed image state is not taken in account yet.

--- CPU Indicator/Views and Controllers/IndicatorMenuBarController.swift
+++ CPU Indicator/Views and Controllers/IndicatorMenuBarController.swift
@@ -12,18 +12,7 @@ import Cocoa
 
 class IndicatorMenuBarController : NSObject, CPUUsageObserver {
 	
-	@IBOutlet private var menu: NSMenu!
-	
-	private let menuBarHeight = NSStatusBar.systemStatusBar().thickness
-	
-	private var statusItems = [NSStatusItem]()
-	
-	private var observingUDC = false
-	private let observedUDCKeys = [
-		"values.\(kUDK_ShowMenuIndicator)",
-		"values.\(kUDK_MenuIndicatorOnePerCPU)", "values.\(kUDK_MenuIndicatorMode)",
-		"values.\(kUDK_MixedImageState)"
-	]
+	@IBOutlet var menu: NSMenu!
 	
 	deinit {
 		if observingUDC {
@@ -79,7 +68,7 @@ class IndicatorMenuBarController : NSObject, CPUUsageObserver {
 				super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
 			}
 		} else if kp == "selectedSkinObjectID" && object === AppDelegate.sharedAppDelegate {
-			(/*TODO*/)
+			updateSkin()
 		} else {
 			super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
 		}
@@ -97,13 +86,20 @@ class IndicatorMenuBarController : NSObject, CPUUsageObserver {
 			let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
 			statusItem.menu = (menu.copy() as! NSMenu)
 			statusItem.menu?.itemWithTag(42)?.title = (oneMenuPerCPU ? String(format: NSLocalizedString("proc %lu", comment: ""), i+1) : NSLocalizedString("global cpu usage", comment: ""))
-			updateStatusItem(statusItem, forProcAtIndex: (oneMenuPerCPU ? i : nil))
-			statusItems.append(statusItem)
+			statusItem.button?.imagePosition = .ImageLeft
+			
+			let layer = CALayer()
+			statusItem.button?.wantsLayer = true
+			statusItem.button?.layer?.addSublayer(layer)
+			
+			let fullItem = (item: statusItem, skinLayer: layer)
+			updateStatusItem(fullItem, forProcAtIndex: (oneMenuPerCPU ? i : nil))
+			statusItems.append(fullItem)
 		}
 	}
 	
 	private func hideIndicatorsIfNeeded() {
-		for statusItem in statusItems {
+		for (statusItem, _) in statusItems {
 			NSStatusBar.systemStatusBar().removeStatusItem(statusItem)
 		}
 		statusItems.removeAll()
@@ -125,6 +121,56 @@ class IndicatorMenuBarController : NSObject, CPUUsageObserver {
 	   MARK: - Private
 	   *************** */
 	
+	private var observingUDC = false
+	private let observedUDCKeys = [
+		"values.\(kUDK_ShowMenuIndicator)",
+		"values.\(kUDK_MenuIndicatorOnePerCPU)", "values.\(kUDK_MenuIndicatorMode)",
+		"values.\(kUDK_MixedImageState)"
+	]
+	
+	private let menuBarHeight = NSStatusBar.systemStatusBar().thickness
+	private let vSpacing = CGFloat(1)
+	
+	private var statusItems = [(item: NSStatusItem, skinLayer: CALayer)]()
+	
+	private var sizedSkin: SizedSkin?
+	private var emptyImageForItem: NSImage?
+	
+	private func updateSkin() {
+		let appDelegate = AppDelegate.sharedAppDelegate
+		let context = appDelegate.mainManagedObjectContext
+		
+		emptyImageForItem = nil
+		
+		guard let selectedSkinObjectId = appDelegate.selectedSkinObjectID else {
+			return
+		}
+		
+		context.performBlockAndWait {
+			guard let skin = (try? context.existingObjectWithID(selectedSkinObjectId)) as? Skin else {
+				return
+			}
+			
+			let r = CGFloat(skin.width) / CGFloat(skin.height)
+			let h = self.menuBarHeight - 2*self.vSpacing
+			let w = h * r
+			self.sizedSkin = SizedSkin(skin: skin, size: NSSize(width: w, height: h), allowDistortion: false)
+		}
+		
+		if let sizedSkin = sizedSkin {
+			emptyImageForItem = NSImage(size: sizedSkin.size)
+			for (_, layer) in statusItems {
+				layer.frame = CGRect(x: 3 /* Magic number (visually pleasant) */, y: self.vSpacing, width: sizedSkin.size.width, height: sizedSkin.size.height)
+			}
+		}
+		
+		/* Updating the status items if they show the image. */
+		let mode = NSUserDefaults.standardUserDefaults().integerForKey(kUDK_MenuIndicatorMode)
+		if mode == MenuIndicatorMode.Image.rawValue || mode == MenuIndicatorMode.Both.rawValue {
+			updateStatusItems()
+		}
+	}
+	
 	private func updateStatusItems() {
 		let oneMenuPerCPU = NSUserDefaults.standardUserDefaults().boolForKey(kUDK_MenuIndicatorOnePerCPU)
 		for (i, statusItem) in statusItems.enumerate() {
@@ -132,10 +178,21 @@ class IndicatorMenuBarController : NSObject, CPUUsageObserver {
 		}
 	}
 	
-	private func updateStatusItem(statusItem: NSStatusItem, forProcAtIndex procIndex: Int?) {
+	private func updateStatusItem(statusItem: (item: NSStatusItem, skinLayer: CALayer), forProcAtIndex procIndex: Int?) {
 		let mode = NSUserDefaults.standardUserDefaults().integerForKey(kUDK_MenuIndicatorMode)
 		let load = (procIndex != nil ? CPUUsageGetter.sharedCPUUsageGetter.cpuUsages[procIndex!] : CPUUsageGetter.sharedCPUUsageGetter.globalCPUUsage)
-		statusItem.button?.title = (mode == MenuIndicatorMode.Text.rawValue || mode == MenuIndicatorMode.Both.rawValue ? String(format: NSLocalizedString("%lu%%", comment: ""), Int(load*100 + 0.5)) : "")
+		
+		/* Updating text */
+		statusItem.item.button?.title = (mode == MenuIndicatorMode.Text.rawValue || mode == MenuIndicatorMode.Both.rawValue ? String(format: NSLocalizedString("%lu%%", comment: ""), Int(load*100 + 0.5)) : "")
+		
+		/* Updating image */
+		if mode == MenuIndicatorMode.Image.rawValue || mode == MenuIndicatorMode.Both.rawValue, let sizedSkin = sizedSkin, cgimage = sizedSkin.imageForProgress(Float(load)).CGImageForProposedRect(nil, context: nil, hints: nil) {
+			statusItem.item.button?.image = emptyImageForItem
+			statusItem.skinLayer.contents = cgimage
+		} else {
+			statusItem.item.button?.image = nil
+			statusItem.skinLayer.contents = nil
+		}
 	}
 	
 }
